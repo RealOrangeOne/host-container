be54cb908387f04a0c989e0b62bb78eac08cfecb
Jake Howard <git@theorangeone.net>
2016-05-16 13:07:18 +0100

    make server more like normal servers

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const PORT = process.env.PORT;
-const serveDir = __dirname + '/site/';
+const serveDir = __dirname + '/site';
+
+const directory = /\/$/;
+const allFiles = /.*/;
 
 const expressConfig = {
   dotfiles: 'deny',
@@ -11,8 +14,21 @@ const expressConfig = {
 
 const app = express();
 
+app.use(function (request, response, next) {
+  // If path is directory then serve index.html
+  if (directory.exec(request.url)) {
+    request.url += '/index.html';
+  }
+  next();
+});
+
 app.use(
   express.static(serveDir, expressConfig)
+);
+
+// Cannot find any file
+app.use(
+  allFiles, express.static(serveDir + '/core/404.html', expressConfig)
 );
 
 const server = app.listen(PORT, function () {
