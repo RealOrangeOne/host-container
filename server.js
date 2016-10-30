01fbdbbfe84eb1c7f6223d17fa68f4c16ac07d91
Jake Howard <git@theorangeone.net>
2016-10-30 12:02:53 +0000

    Add logging

--- server.js
+++ server.js
@@ -3,33 +3,46 @@ const staticFile = require('connect-static-file');
 const compression = require('compression');
 const helmet = require('helmet');
 const path = require('path');
+const winston = require('winston');
+const expressWinston = require('express-winston');
 
 const PORT = process.env.PORT || 5000;
 const SERVE_DIR = path.join(__dirname, '/site');
 const PAGE_404 = path.join(SERVE_DIR, '.404.html');
-
-const directory = /\/$/;
-
-const expressConfig = {
+const EXPRESS_CONFIG = {
   dotfiles: 'ignore',
   index: false,
   redirect: true
 };
+const LOGGER_MESSAGE = '{{ req.url }}'
+  .concat('status:{{ res.statusCode }} ')
+  .concat('useragent:{{ req.headers["user-agent"] }} ')
+  .concat('time:{{ res.responseTime }}ms');
 
 const app = express();
 
 app.use(compression({ level: 9 }));
 app.use(helmet());
-
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console({
+      colorize: true
+    })
+  ],
+  meta: false,
+  msg: LOGGER_MESSAGE,
+  colorize: true,
+  statusLevels: true
+}));
 
 app.use(function (request, response, next) {
-  if (directory.exec(request.url)) {
+  if (request.url.endsWith('/')) {
     request.url = path.join(request.url, 'index.html');
   }
   next();
 });
 
-app.use(express.static(SERVE_DIR, expressConfig));
+app.use(express.static(SERVE_DIR, EXPRESS_CONFIG));
 
 app.use(function (request, response, next) {
   response.statusCode = 404;
