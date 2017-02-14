4189d95b8bfca736906b63da645c268a2618d10c
Jake Howard <git@theorangeone.net>
2017-02-14 21:24:35 +0000

    Remove old files

--- src/404.js
+++
@@ -1,9 +0,0 @@
-const staticFile = require('connect-static-file');
-const path = require('path');
-const { SERVE_DIR } = require('./consts');
-
-const handle404 = staticFile(path.join(SERVE_DIR, '.404.html'));
-module.exports = function (request, response, next) {
-  response.statusCode = 404;
-  return handle404(request, response, next);
-};
