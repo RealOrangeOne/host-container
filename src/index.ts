00e00ad7495e437d0cd50fcd6e4c560939fa4dbb
Jake Howard <git@theorangeone.net>
2017-02-17 21:39:44 +0000

    export server

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,10 @@ console.log("Starting Server...");
 const ARGS = getArgs();
 const app = createServer(ARGS);
 
-const server = app.listen(ARGS.port, function () {
+export const server = app.listen(ARGS.port, function () {
     const port = server.address().port;
     console.log("Server started on port " + port);
     if (ARGS.open) {
         open('http://0.0.0.0:' + port);
     }
 });
-
