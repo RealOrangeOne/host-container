8181a4221d8421ff2a37c2b4cce30e8c9f9cc832
Jake Howard <git@theorangeone.net>
2017-02-27 10:18:45 +0000

    Test custom 404 page

--- tests/middleware/404.test.ts
+++ tests/middleware/404.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import { runServer } from '../helpers';
-
+import * as fs from 'fs';
+import * as path from 'path';
 
 describe('404 Middleware', function () {
     it('Should respond with 404 on bad path', function (done) {
@@ -16,5 +17,31 @@ describe('404 Middleware', function () {
            expect(response.status).to.equal(404);
            expect(response.text()).to.eventually.include('Cannot GET').notify(done);
        });
+    });
+
+    describe('Custom 404 Page', function () {
+        const CUSTOM_PAGE = path.join(__dirname, '..', '..', 'site', '.404.html')
+        beforeEach(function () {
+            fs.writeFileSync(CUSTOM_PAGE, 'Custom 404 Page');
+        });
+
+        afterEach(function () {
+            fs.unlinkSync(CUSTOM_PAGE);
+        });
+
+        it('should have custom page', function (done) {
+            runServer({
+                allowed_ips: [],
+                basicAuth: [],
+                dirList: false,
+                serveDir: 'site/',
+                opbeat: false,
+                open: false
+            }, '/foo/bar', function (response : any) {
+                expect(response.ok).to.be.false;
+                expect(response.status).to.equal(404);
+                expect(response.text()).to.eventually.include('Custom 404 Page').notify(done);
+            });
+        });
     });
 });
